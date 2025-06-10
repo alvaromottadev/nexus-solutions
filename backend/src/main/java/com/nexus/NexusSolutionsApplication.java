fd842a0a4e7cc75143c48a0360418b0f92fab19c
motta <alvaromotta.dev@gmail.com>
2025-06-10 18:19:10 -0300

    chore: initial backend commit

---
+++ backend/src/main/java/com/nexus/NexusSolutionsApplication.java
@@ -0,0 +1,13 @@
+package com.nexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusSolutionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusSolutionsApplication.class, args);
+	}
+
+}
