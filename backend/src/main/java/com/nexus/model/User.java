e00fe1e2c739cd4d249d9013e1a9b6d105d269c7
motta <alvaromotta.dev@gmail.com>
2025-06-11 18:37:57 -0300

    feat: add Lombok annotations to User entity for automatic getter and setter generation

--- backend/src/main/java/com/nexus/model/User.java
+++ backend/src/main/java/com/nexus/model/User.java
@@ -2,9 +2,13 @@ package com.nexus.model;
 
 import com.nexus.model.enums.UserType;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "users")
 public class User {
