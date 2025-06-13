f1c46f90344a4ed8dd6c88f599ebbf4abafbe392
motta <alvaromotta.dev@gmail.com>
2025-06-12 21:35:39 -0300

    feat: add validation for name field in ProductRequest DTO

--- backend/src/main/java/com/nexus/dto/Product/ProductRequest.java
+++ backend/src/main/java/com/nexus/dto/Product/ProductRequest.java
@@ -1,9 +1,14 @@
 package com.nexus.dto.Product;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ProductRequest(
 
+        @NotBlank(message = "Name is required")
         String name,
+
         String description,
+
         String qrCode
 
 ) {
