9eeffda092887ae34b64e4b4846f90a1905776c2
motta <alvaromotta.dev@gmail.com>
2025-06-14 20:45:02 -0300

    feat: enhance styling with Tailwind CSS, add form components, and update LoginPage layout

---
+++ frontend/src/components/Login/LoginForm.tsx
@@ -0,0 +1,83 @@
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+
+export default function LoginForm() {
+  const form = useForm();
+
+  return (
+    <div className="mt-[12.75rem] flex justify-center">
+      <div className="w-[70%]">
+        <Form {...form}>
+          <FormField
+            control={form.control}
+            name="email"
+            render={(field) => (
+              <FormItem>
+                <FormLabel className="text-[1rem] font-poppins">
+                  Email
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="bg-[var(--color-gray)] text-white placeholder:text-[rgba(255,255,255,0.5)] font-poppins h-[4rem]"
+                    placeholder="Ex.: nexus@gmail.com"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={(field) => (
+              <FormItem>
+                <FormLabel className="text-[1rem] font-poppins">
+                  Password
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    className="bg-[var(--color-gray)] text-white placeholder:text-[rgba(255,255,255,0.5)] font-poppins h-[4rem]"
+                    placeholder="Ex.: nexus123"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription />
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="w-full flex justify-end font-poppins">
+            <label>
+              Esqueceu a senha?{" "}
+              <label className="text-[var(--primary-color)]">
+                <label>Clique aqui!</label>
+              </label>
+            </label>
+          </div>
+          <Button className="w-full h-[4rem] mt-[0.25rem] bg-[var(--primary-color)]">
+            Login
+          </Button>
+        </Form>
+        <div className="w-full flex justify-center mt-[0.25rem] font-poppins">
+          <label>
+            Não possui uma conta?{" "}
+            <label className="text-[var(--primary-color)]">Cadastre-se!</label>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,11 +1,13 @@
 import LoginHeader from "../components/Login/LoginHeader";
-import LoginBody from "../components/LoginBody";
+import LoginBody from "../components/Login/LoginBody";
+import LoginForm from "@/components/Login/LoginForm";
 
 export default function LoginPage() {
   return (
-    <>
+    <div className="bg-[var(--background-color)]">
       <LoginHeader />
       <LoginBody />
-    </>
+      <LoginForm />
+    </div>
   );
 }
