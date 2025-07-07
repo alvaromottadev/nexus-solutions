ae58b05856d7153ffe24c7059822b137b390479e
motta <alvaromotta.dev@gmail.com>
2025-07-07 20:43:39 -0300

    feat: personalize greeting in HomeBody; display company name instead of static text and update service message for clarity

--- frontend/src/components/Home/HomeBody.tsx
+++ frontend/src/components/Home/HomeBody.tsx
@@ -1,20 +1,25 @@
+import { useAuth } from "@/hooks/useAuth";
 import Contacts from "../Contacts";
 import CustomText from "../CustomText";
 import GraphicComponent from "../GraphicComponent";
 
 export default function HomeBody() {
+  const auth = useAuth();
+
+  const companyName = auth?.user?.company.name;
+
   return (
     <>
       <div className="flex flex-col flex-1 items-center justify-center">
         <div className="flex justify-center lg:justify-between p-25 w-full">
           <div className="flex flex-col items-center lg:items-start w-[80%] gap-y-2">
             <CustomText className="text-[2.5rem] text-[var(--primary-color)] font-bold lg:text-left">
-              Olá, Meliy!
+              Olá, {companyName}!
             </CustomText>
 
             <CustomText className="text-[1.5rem] text-center lg:text-left lg:w-[30rem] lg:text-[2rem]">
-              Obrigado por confiar em nossa empresa! Estamos aqui para oferecer
-              as melhores soluções e apoiar seu sucesso.
+              Obrigado por confiar em nossos serviços! Estamos aqui para
+              oferecer as melhores soluções e apoiar seu sucesso.
             </CustomText>
             <Contacts />
           </div>
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,11 +1,5 @@
-import CustomText from "@/components/CustomText";
 import HomeBody from "@/components/Home/HomeBody";
 import TopBar from "@/components/TopBar";
-import type { CompanyType } from "@/types/CompanyType";
-
-interface HomePageProps {
-  company: CompanyType;
-}
 
 export default function HomePage() {
   return (
