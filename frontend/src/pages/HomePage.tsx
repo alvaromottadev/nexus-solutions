559b5e2b0024cb86df59382ced3838345195177c
motta <alvaromotta.dev@gmail.com>
2025-07-19 14:30:58 -0300

    feat: improve layout and styling in HomeBody, HomePage, InitialSection, and update index.html

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -4,7 +4,7 @@ import TopBar from "@/components/TopBar";
 export default function HomePage() {
   return (
     <>
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-screen w-full flex flex-col">
         <TopBar />
         <HomeBody />
       </div>
