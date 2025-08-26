244f9209274dea6e46e34ae17e36ff9d75235ca3
motta <alvaromotta.dev@gmail.com>
2025-08-26 08:07:46 -0300

    feat: add new component to read bar code in MovementToolPage

---
+++ frontend/src/components/SearchToolComponent.tsx
@@ -0,0 +1,38 @@
+
+import { Input } from "./ui/input";
+
+interface SearchComponentToolProps {
+  label: string;
+  placeholder: string;
+  handleSearch: () => void;
+  setValue: (name: string) => void;
+  inputDisabled?: boolean;
+  value: string;
+}
+
+export default function SearchComponentTool({
+  placeholder,
+  handleSearch,
+  setValue,
+  inputDisabled = false,
+  value
+}: SearchComponentToolProps) {
+  return (
+    <>
+      <div className="w-[90%] flex gap-x-3 items-center justify-center">
+        <Input
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSearch();
+            }
+          }}
+          onChange={(e) => setValue(e.target.value)}
+          placeholder={placeholder}
+          className="h-[2.5rem] w-[80%] text-black font-poppins placeholder:font-poppins"
+          disabled={inputDisabled}
+          value={value}
+        />
+      </div>
+    </>
+  );
+}
