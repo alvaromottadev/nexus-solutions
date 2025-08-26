1683878b3caed7856560be8716130e5e7bb25adc
motta <alvaromotta.dev@gmail.com>
2025-08-26 07:57:46 -0300

    feat: add state defaultValue in SearchComponent

--- frontend/src/components/SearchComponent.tsx
+++ frontend/src/components/SearchComponent.tsx
@@ -1,7 +1,9 @@
+
 import { Search } from "lucide-react";
 import CustomText from "./CustomText";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 interface SearchComponentProps {
   label: string;
@@ -9,6 +11,7 @@ interface SearchComponentProps {
   handleSearch: () => void;
   setName: (name: string) => void;
   inputDisabled?: boolean;
+  value?: string;
 }
 
 export default function SearchComponent({
@@ -17,7 +20,11 @@ export default function SearchComponent({
   handleSearch,
   setName,
   inputDisabled = false,
+  value = ""
 }: SearchComponentProps) {
+
+  const [defaultValue, setDefaultValue] = useState<string>(value);
+
   return (
     <>
       <CustomText className="text-[var(--primary-color)] text-[2rem] md:text-[2.5rem] font-bold">
@@ -30,10 +37,14 @@ export default function SearchComponent({
               handleSearch();
             }
           }}
-          onChange={(e) => setName(e.target.value)}
+          onChange={(e) => {
+            setDefaultValue(e);
+            setName(value);
+          }}
           placeholder={placeholder}
           className="h-[2.5rem] w-[80%] text-black font-poppins placeholder:font-poppins"
           disabled={inputDisabled}
+          value={value}
         />
         <Button
           className="h-[2.5rem] w-[2.5rem] bg-transparent shadow-none hover:bg-transparent cursor-pointer"
