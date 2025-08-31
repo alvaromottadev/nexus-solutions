5c21ba3088dd8e5e2a17143838a6e093196d0749
motta <alvaromotta.dev@gmail.com>
2025-08-31 11:26:13 -0300

    feat: enable annotation @EnableCaching

--- backend/src/main/java/com/nexus/NexusSolutionsApplication.java
+++ backend/src/main/java/com/nexus/NexusSolutionsApplication.java
@@ -2,6 +2,7 @@ package com.nexus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -9,6 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 @EnableAsync
 public class NexusSolutionsApplication {
 
